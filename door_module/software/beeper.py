e125d4eb90b29be9ac6105721b73a540a21b1319
Patrick Himmelmann <patrick.himmelmann@kit.edu>
2019-02-03 16:50:11 +0000

    Have beepstyles

--- door_module/software/beeper.py
+++ door_module/software/beeper.py
@@ -5,19 +5,19 @@ from datetime import datetime
 class Beeper:
 	def __init__(self, beep):
 		self.beep=beep
+		self.tunes={
+				'pin':[0b11010101,0b11000001],
+				'deny':[0b11010101,0b11000001],
+				'attention':[0b11010101,0b11000001],
+				'keypress':[0b11010101,0b11000001],
+				'confirm':[0b11010101,0b11000001],
+				}
+
+	def beep_by_style(self,style):
+		self.beep(bytearray(self.tunes[style]))
 
 	def confirm(self):
-		tune=[0b11000001]
-		self.beep(bytearray(tune))
+		self.beep_by_style('confirm')
 
 	def keypress(self):
-		tune=[0b11010101]
-		self.beep(bytearray(tune))
-
-	def ack(self):
-		tune=[0b11010101,0b11000001]
-		self.beep(bytearray(tune))
-
-	def nak(self):
-		tune=[0b11000001,0b11011011]
-		self.beep(bytearray(tune))
+		self.beep_by_style('keypress')
