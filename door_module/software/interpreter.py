5ff2521f9aae5a90710ecf5da3d827890239c969
Patrick Himmelmann <himmelmannp@gmail.com>
2019-11-05 19:02:10 +0000

    Add control of automatic door closer

--- door_module/software/interpreter.py
+++ door_module/software/interpreter.py
@@ -1,14 +1,17 @@
 from logging import warn
 
 class Interpreter:
-	def __init__(self, opener, beeper_outside,beeper_inside):
+	def __init__(self, opener, closer, beeper_outside,beeper_inside):
 		self.opener=opener
+		self.closer=closer
 		self.beeper_inside=beeper_inside
 		self.beeper_outside=beeper_outside
 
 	def do(self,command):
 		if command['type']=='open':
 			self.opener()
+		elif command['type']=='close':
+			self.closer()
 		elif command['type']=='beep':
 			style=command['beepstyle']
 			if command['location']=='inside':
